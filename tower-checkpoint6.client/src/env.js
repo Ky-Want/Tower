b734f5ab653e279975ca70f379dd8a3e0bac3b9c
Lumine3449 <>
2022-10-04 17:06:27 -0600

    Not workspace env

--- tower-checkpoint6.client/src/env.js
+++ tower-checkpoint6.client/src/env.js
@@ -1,6 +1,7 @@
 export const dev = window.location.origin.includes('localhost')
 export const baseURL = dev ? 'http://localhost:3000' : ''
 export const useSockets = false
-export const domain = ''
-export const clientId = ''
-export const audience = ''
+export const domain = 'https://kywant.us.auth0.com/api/v2/'
+export const clientId = '7C3MrrEqS1d1xPGDoGXG4tsQkIXv5XaE'
+export const audience = 'https://classroom-demos.com'
+
