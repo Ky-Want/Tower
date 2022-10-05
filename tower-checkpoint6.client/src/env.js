5fac18c4bf4893eabb3e2a69273937f1c5f93781
Lumine3449 <>
2022-10-04 20:20:35 -0600

    tower push

--- tower-checkpoint6.client/src/env.js
+++ tower-checkpoint6.client/src/env.js
@@ -1,7 +1,7 @@
 export const dev = window.location.origin.includes('localhost')
 export const baseURL = dev ? 'http://localhost:3000' : ''
 export const useSockets = false
-export const domain = 'https://kywant.us.auth0.com/api/v2/'
+export const domain = 'kywant.us.auth0.com'
 export const clientId = '7C3MrrEqS1d1xPGDoGXG4tsQkIXv5XaE'
 export const audience = 'https://classroom-demos.com'
 
