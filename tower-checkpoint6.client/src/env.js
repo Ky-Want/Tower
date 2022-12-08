29006c4334a8662bcb56d6688bdd6436ecad57cc
Lumine3449 <>
2022-12-08 15:06:39 -0700

    last mongo db was deleted, had to update

--- tower-checkpoint6.client/src/env.js
+++ tower-checkpoint6.client/src/env.js
@@ -1,7 +1,6 @@
 export const dev = window.location.origin.includes('localhost')
-export const baseURL = dev ? 'http://localhost:3000' : ''
+export const baseURL = dev ? 'http://localhost:3000/' : ''
 export const useSockets = false
 export const domain = 'kywant.us.auth0.com'
-export const clientId = '7C3MrrEqS1d1xPGDoGXG4tsQkIXv5XaE'
-export const audience = 'https://classroom-demos.com'
-
+export const clientId = 'LTPyVFpFD5y9fMjayzDrZvOuLg6hFsbn'
+export const audience = 'https://loreheim/'
